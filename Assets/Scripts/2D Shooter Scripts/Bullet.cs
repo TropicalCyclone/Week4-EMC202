ec946932fa2f5adfdd684020907668d5a5137827
ZEPHYRUS-G14\Kyle Logdat <logdatkyle@gmail.com>
2022-12-07 14:26:03 +0800

    Created Basic 3rd Person Shooter

---
+++ Assets/Scripts/2D Shooter Scripts/Bullet.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Wall":
+                Destroy(gameObject);
+                break;
+            case "Enemy":
+                Destroy(gameObject);
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/2D Shooter Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private float fireForce = 20f;
+
+    public void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+    }
+}
